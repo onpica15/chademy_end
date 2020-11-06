f9494de2a8b9f00166fe3dafdea063aeef039d38
Eva1009 <tctct785@gmail.com>
2020-11-06 16:12:01 +0800

    fund

--- src/routes/man_fund.js
+++ src/routes/man_fund.js
@@ -123,22 +123,16 @@ router.get("/list", async (req, res) => {
 });
 
 router.get("/fundlistnode", async (req, res) => {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
   let sql = `SELECT * FROM e_fund_project ORDER BY sid`;
-=======
-  let sql = `SELECT * FROM e_fund_project ORDER BY sid  LIMIT 10`;
->>>>>>> Stashed changes
-=======
-  let sql = `SELECT * FROM e_fund_project ORDER BY sid  LIMIT 10`;
->>>>>>> Stashed changes
+
+ 
   const [results] = await db.query(sql); 
   res.json(results);
 
 });
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+
 // funditem
 router.get("/reactitem/:sid", async (req, res) => {
   
@@ -148,10 +142,8 @@ router.get("/reactitem/:sid", async (req, res) => {
 });
 
 
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
+
 // get 追蹤狀態
 router.get("/heart/:sid", async (req, res) => {
   
@@ -303,13 +295,7 @@ router.post('/add', upload.none(), async (req, res) => {
 
 
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
   const sql = "INSERT INTO `e_fund_project` set ?";
   const [{
     affectedRows,
@@ -328,15 +314,10 @@ router.post('/add', upload.none(), async (req, res) => {
 
 // 資料刪除 API
 router.delete("/del/:sid", async (req, res) => {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
+ Updated upstream
   const sql = "DELETE FROM `e_fund_project` WHERE sid=?";
-=======
-  const sql = "DELETE FROM `we_fund_project` WHERE sid=?";
->>>>>>> Stashed changes
-=======
-  const sql = "DELETE FROM `we_fund_project` WHERE sid=?";
->>>>>>> Stashed changes
+
+
   const [results] = await db.query(sql, [req.params.sid]);
   res.json(results);
 });
